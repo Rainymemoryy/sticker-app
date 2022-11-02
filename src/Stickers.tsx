9ee51f3ed1aeae17b79223ed0628c67586929ad1
DucKhang <63650705+Rainymemoryy@users.noreply.github.com>
2022-11-02 14:03:52 +0700

    sssadsa

--- src/Stickers.tsx
+++ src/Stickers.tsx
@@ -3,19 +3,24 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import {
   Canvas,
+  DashPathEffect,
+  DiscretePathEffect,
+  Group,
+  Paint,
+  Path,
   Skia,
   useFont,
   useImage,
-  useTouchHandler,
   useValue,
 } from "@shopify/react-native-skia";
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { Dimensions, SafeAreaView, View } from "react-native";
 
 import { GestureHandler } from "./GestureHandler";
 import { GestureHandlerV2 } from "./GestureHandlerV2";
 import { HelloSticker, HelloStickerDimensions } from "./HelloSticker";
 import { LocationSticker, LocationStickerDimensions } from "./LocationSticker";
+import type { DrawingElement, DrawingElements } from "./types";
 import { useTouchDrawing } from "./useTouchDrawing";
 
 const { width, height } = Dimensions.get("window");
@@ -24,9 +29,76 @@ const zurich = require("./assets/zurich.jpg");
 const aveny = require("./assets/aveny.ttf");
 
 export const Stickers = () => {
-  const touchHandle = useTouchDrawing();
   const helloMatrix = useValue(Skia.Matrix());
   const locationMatrix = useValue(Skia.Matrix());
+  const [elements, setElements] = useState<DrawingElements>();
+  const touchHandle = useTouchDrawing();
+
+  const elementComponents = useMemo(
+    () =>
+      elements?.map((element: DrawingElement, index) => {
+        switch (element.type) {
+          case "image":
+            return (
+              // <Image
+              //   fit="fill"
+              //   key={index}
+              //   image={element.image}
+              //   rect={() => getBounds(element)}
+              // />
+              <></>
+            );
+          default:
+            switch (element.pathType) {
+              case "discreted":
+                return (
+                  <Group key={index}>
+                    <Paint style="stroke" strokeWidth={4}>
+                      <DiscretePathEffect length={3} deviation={5} />
+                    </Paint>
+                    <Path
+                      path={element.path}
+                      color={element.color}
+                      style="stroke"
+                      strokeWidth={element.size}
+                      strokeCap="round"
+                    />
+                  </Group>
+                );
+              case "dashed":
+                return (
+                  <Group key={index}>
+                    <Paint style="stroke" strokeWidth={4}>
+                      <DashPathEffect
+                        intervals={[element.size * 2, element.size * 2]}
+                      />
+                    </Paint>
+                    <Path
+                      path={element.path}
+                      color={element.color}
+                      style="stroke"
+                      strokeWidth={element.size}
+                      strokeCap="round"
+                    />
+                  </Group>
+                );
+              default:
+                return (
+                  <Path
+                    key={index}
+                    path={element.path}
+                    color={element.color}
+                    style="stroke"
+                    strokeWidth={element.size}
+                    strokeCap="round"
+                  />
+                );
+            }
+        }
+      }),
+    [elements]
+  );
+
   const image = useImage(zurich);
   const font = useFont(aveny, 56);
   if (!image || !font) {
@@ -40,6 +112,7 @@ export const Stickers = () => {
           style={{ width, height, backgroundColor: "red" }}
           onTouch={touchHandle}
         >
+          {elementComponents}
           <HelloSticker matrix={helloMatrix} />
           <LocationSticker font={font} matrix={locationMatrix} />
         </Canvas>
--- src/useTouchDrawing.tsx
+++ src/useTouchDrawing.tsx
@@ -1,16 +1,72 @@
+/* eslint-disable import/order */
 /* eslint-disable prettier/prettier */
-import { useTouchHandler } from "@shopify/react-native-skia";
+
+import { SkPoint, useTouchHandler } from "@shopify/react-native-skia";
+import { useRef } from "react";
+import type { DrawingElements } from "./types";
+
+interface Props {
+  setElements: React.Dispatch<
+    React.SetStateAction<DrawingElements | undefined>
+  >;
+}
 
 export const useTouchDrawing = () => {
+  const prevPointRef = useRef<SkPoint>();
+  // const drawContext = useDrawContext();
+  // const uxContext = useUxContext();
+
   return useTouchHandler({
     onStart: ({ x, y }) => {
-      console.log("start", x, y);
+      // switch (uxContext.state.menu) {
+      //   case undefined:
+      //   case "drawing":
+      //   case "chooseSticker":
+      //   case "colors": {
+      //     const { color, size, pathType } = drawContext?.state;
+      //     drawContext.commands.addElement(
+      //       createPath(x, y, color, size, pathType)
+      //     );
+      //     break;
+      //   }
+
+      //   default:
+      //     break;
+      // }
+      prevPointRef.current = { x, y };
     },
     onActive: ({ x, y }) => {
-      console.log("move", x, y);
-    },
-    onEnd: () => {
-      console.log("end");
+      // switch (uxContext.state.menu) {
+      //   case undefined:
+      //   case "drawing":
+      //   case "chooseSticker":
+      //   case "colors": {
+      //     const element =
+      //       drawContext.state.elements[drawContext.state.elements.length - 1];
+      //     const xMid = (prevPointRef.current!.x + x) / 2;
+      //     const yMid = (prevPointRef.current!.y + y) / 2;
+      //     element?.path?.quadTo(
+      //       prevPointRef.current!.x,
+      //       prevPointRef.current!.y,
+      //       xMid,
+      //       yMid
+      //     );
+      //     break;
+      //   }
+      //   default:
+      //     break;
+      // }
+      // const element =
+      //   drawContext.state.elements[drawContext.state.elements.length - 1];
+      // const xMid = (prevPointRef.current!.x + x) / 2;
+      // const yMid = (prevPointRef.current!.y + y) / 2;
+      // element?.path?.quadTo(
+      //   prevPointRef.current!.x,
+      //   prevPointRef.current!.y,
+      //   xMid,
+      //   yMid
+      // );
+      // prevPointRef.current = { x, y };
     },
   });
 };
